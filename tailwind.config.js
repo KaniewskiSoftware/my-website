7ea070127b252e0724b944071d0f88678df63181
KaniewskiSoftware <117393647+st4rkmano@users.noreply.github.com>
2023-04-24 11:21:58 +0200

    initial settings, theme, fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          200: "#526D89",
+          300: "#3B546C",
+          400: "#2C3E50",
+          500: "#1F2D3C",
+          600: "#111E27",
+        },
+        secondary: {
+          200: "#6AB2F0",
+          300: "#4D9FDE",
+          400: "#3498DB",
+          500: "#217AC8",
+          600: "#1260A3",
+        },
+        accent: "#ECF0F1",
+        text: "#333333",
       },
     },
   },
   plugins: [],
-}
+};
