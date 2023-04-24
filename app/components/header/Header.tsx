b4b26aca826b4a6d158c6c5a08b7e1f24eee1996
KaniewskiSoftware <117393647+st4rkmano@users.noreply.github.com>
2023-04-24 14:50:30 +0200

    logo responsivness fix

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
     setIsOpen(false);
   };
   return (
-    <header className="relative flex justify-between px-2 w-full bg-primary-400 text-white">
+    <header className="relative flex justify-between items-center px-2 w-full bg-primary-400 text-white lg:pl-6 lg:pr-4 xl:text-lg">
       <Logo />
       <Nav />
       <BurgerButton onClick={toggleNav} isOpen={isOpen} />
--- app/components/header/Logo.tsx
+++ app/components/header/Logo.tsx
@@ -6,15 +6,16 @@ import { useRouter } from "next/navigation";
 const Logo = () => {
   const router = useRouter();
   return (
-    <Image
-      className="cursor-pointer p-2"
-      src="/images/logo.png"
-      alt="Logo"
-      width={40}
-      height={40}
-      sizes="35px"
-      priority
-    />
+    <div className="relative w-[66.48px] h-[48px] xl:w-[83.1px] xl:h-[60px]">
+      <Image
+        className="cursor-pointer p-2.5"
+        src="/images/logo.png"
+        alt="Logo"
+        fill
+        sizes="66.5px, (max-width: 1280px) 83.1px"
+        priority
+      />
+    </div>
   );
 };
 export default Logo;
